c98ed609316871febe3b24a1c82d866d3a1e6265
Teoneko <ruslioncheg@gmail.com>
2022-04-22 14:00:06 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,73 +1,69 @@
 <?php
-// Файлы phpmailer
-require 'phpmailer/PHPMailer.php';
-require 'phpmailer/SMTP.php';
-require 'phpmailer/Exception.php';
+    use PHPMailer\PHPmailer\PHPmailer;
+    use PHPMailer\PHPmailer\Exception;
 
-// Переменные, которые отправляет пользователь
-$name = $_POST['name'];
-$email = $_POST['mail'];
-$subject = $_POST['subject'];
-$text = $_POST['message'];
-$file = $_FILES['file-image'];
 
-// Формирование самого письма
-$title = $subject;
-$body = "
-<h2>Новое письмо</h2>
-<b>Имя:</b> $name<br>
-<b>Почта:</b> $email<br><br>
-<b>Сообщение:</b><br>$text
-";
+    require 'phpmailer/Exception.php';
+    require 'phpmailer/SMTP.php';
+    require 'phpmailer/PHPmailer.php';
 
-// Настройки PHPMailer
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-try {
-    $mail->isSMTP();   
-    $mail->CharSet = "UTF-8";
+
+    $mail = new PHPMailer(true);
+    $mail -> Charset = 'UTF-8';
+    $mail -> setLanguage('ru', 'phpmailer/language/');
+    $mail -> IsHTML(true);
+    $mail->isSMTP();
     $mail->SMTPAuth   = true;
     //$mail->SMTPDebug = 2;
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
-
     // Настройки вашей почты
     $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
     $mail->Username   = 'Mailersendertome@gmail.com'; // Логин на почте
-    $mail->Password   = 'Playeradditem007'; // Пароль на почте
+    $mail->Password   = 'ElfenLied147'; // Пароль на почте
     $mail->SMTPSecure = 'auto';
     $mail->Port       = 587;
     $mail->setFrom('Mailersendertome@gmail.com', 'Postman'); // Адрес самой почты и имя отправителя
-
+    //Тема письма
+    $mail -> Subject = 'This message from Kate\'s site';
     // Получатель письма
-    $mail->addAddress('ruslioncheg@gmail.com');  
+    $mail->addAddress('ruslioncheg@gmail.com'); 
+    
+    //Тело письма
+    $body = '<h1>Ваше супер письмо!</h1>';
 
-    // Прикрипление файлов к письму
-if (!empty($file['name'][0])) {
-    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
-        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-        $filename = $file['name'][$ct];
-        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
-            $mail->addAttachment($uploadfile, $filename);
-            $rfile[] = "Файл $filename прикреплён";
-        } else {
-            $rfile[] = "Не удалось прикрепить файл $filename";
+    if (trim(!empty($_POST['name']))){
+        $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+    }
+    if (trim(!empty($_POST['mail']))){
+        $body.='<p><strong>E-mail:</strong> '.$_POST['mail'].'</p>';
+    }
+    if (trim(!empty($_POST['subject']))){
+        $body.='<p><strong>Тема:</strong> '.$_POST['subject'].'</p>';
+    }
+    if (trim(!empty($_POST['message']))){
+        $body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+    }
+
+    if (!empty($_FILES['image']['tmp_name'])){
+        $filePath = __DIR__ . "/files/" . $_FILES['image']['name'];
+        if(copy($_FILES['image']['tmp_name'], $filePath)){
+            $fileAttach = $filePath;
+            $body.='<p><strong>Фото:</strong></p>';
+            $mail -> addAttachment($fileAttach);
         }
-    }   
-}
-// Отправка сообщения
-$mail->isHTML(true);
-$mail->Subject = $title;
-$mail->Body = $body;    
+    }
 
-// Проверяем отравленность сообщения
-if ($mail->send()) {$message = "Error";} 
-else {$message = "Data sent!";}
+    $mail->Body = $body;
 
-} catch (Exception $e) {
-    $message = "Error";
-    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
-}
+    if (!$mail->send()) {
+        $message = 'Error';
+    } else {
+        $message = 'Data sent';
+    }
 
-// Отображение результата
-header('Content-type: application/json');
-echo json_encode(["response" => $message, "resultfile" => $rfile, "status" => $status]);
+    $response = ['message' => $message];
+    array_map('unlink', glob(dirname(__FILE__).'/files/*'));
+
+    header('Content-type: application/json');
+    echo json_encode($response);
 ?>
